0a6ad8084c799ea214ab5c849e1653d4ef115ec9
Jada <jada.fagan@gmail.com>
2020-06-17 11:23:31 -0400

    Add project files.

---
+++ CsharpTypes/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace CsharpTypes
+{
+    class Program
+    {
+        static void Main(string[] args) {
+
+            Console.WriteLine("Enter first number : ");
+            var nbr1AsString = Console.ReadLine();
+            var nbr1 = Convert.ToInt32(nbr1AsString);
+
+
+            Console.WriteLine("Enter second number : ");
+            var nbr2AsString = Console.ReadLine();
+            var nbr2 = Convert.ToInt32(nbr2AsString);
+
+            var answer = nbr1 + nbr2;
+
+            Console.Write($"{nbr1} plus {nbr2} equals {answer}");
+
+            //Console.Write("Enter some data and press enter: ");
+            //var response = Console.ReadLine();
+            //Console.WriteLine(response);
+        }
+    }
+}
